36df63c01e34fe1130c2f1a59b51274011b51062
murthychiluka <murthychiluka@gmail.com>
2026-08-09 21:54:31 +0530

    added app.py

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,7 +87,7 @@ resource "aws_instance" "runner" {
   instance_type               = "t3.micro"
   subnet_id                   = data.aws_subnets.public.ids[0]
   vpc_security_group_ids      = [aws_security_group.runner_sg.id]
-  key_name                    = "india-key.pem"
+  key_name                    = "india-key"
   associate_public_ip_address = true
   tags = { Name = "github-runner" }
 }
@@ -97,7 +97,7 @@ resource "aws_instance" "app_server" {
   instance_type               = "t3.micro"
   subnet_id                   = data.aws_subnets.public.ids[0]   # same subnet, no public IP = "private"
   vpc_security_group_ids      = [aws_security_group.app_sg.id]
-  key_name                    = "india-key.pem"
+  key_name                    = "india-key"
   associate_public_ip_address = false
   tags = { Name = "app-server" }
 }
